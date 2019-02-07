0f822857acfc2f9ce27cdfb213071b9b684d2eba
Felix Jancso-Szabo <felix.ajs@gmail.com>
2019-02-06 23:42:30 -0800

    added some tests

---
+++ cmd/helpers_test.go
@@ -0,0 +1,78 @@
+package cmd
+
+import (
+	"os/user"
+	"testing"
+
+	a "github.com/stretchr/testify/assert"
+	r "github.com/stretchr/testify/require"
+)
+
+func TestParseHostsArg(t *testing.T) {
+	assert := a.New(t)
+	require := r.New(t)
+
+	u, err := user.Current()
+	require.Nil(err)
+	tests := []struct {
+		input  string
+		output []*host
+		err    bool
+	}{
+		{
+			input: "example.com",
+			output: []*host{
+				{
+					hostName: "example.com",
+					port:     defaultPort,
+					user:     u.Username,
+				},
+			},
+			err: false,
+		},
+		{
+			input: "user@example.com",
+			output: []*host{
+				{
+					hostName: "example.com",
+					port:     defaultPort,
+					user:     "user",
+				},
+			},
+			err: false,
+		},
+		{
+			input: "example.com:30",
+			output: []*host{
+				{
+					hostName: "example.com",
+					port:     30,
+					user:     u.Username,
+				},
+			},
+			err: false,
+		},
+		{
+			input: "user@example.com:30",
+			output: []*host{
+				{
+					hostName: "example.com",
+					port:     30,
+					user:     "user",
+				},
+			},
+			err: false,
+		},
+	}
+
+	for i, t := range tests {
+		o, err := parseHostsArg(t.input)
+
+		assert.Equalf(t.output, o, "failure in test #%v", i)
+		if t.err {
+			assert.Errorf(err, "failure in test #%v", i)
+		} else {
+			assert.NoErrorf(err, "failure in test #%v", i)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/sirupsen/logrus v1.3.0
 	github.com/spf13/cobra v0.0.4-0.20190109003409-7547e83b2d85
 	github.com/spf13/pflag v1.0.1 // indirect
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20190131182504-b8fe1690c613
 	golang.org/x/sys v0.0.0-20190204203706-41f3e6584952 // indirect
 )
